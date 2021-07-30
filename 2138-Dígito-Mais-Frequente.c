bb68ee8cf729acf0b16c4ddac3b7b7cc7c6929fd
Daniel de Souza Lima Junior <39025412+OHomemParede@users.noreply.github.com>
2021-07-30 14:43:02 -0300

    Create 2138-Dígito-Mais-Frequente.c

---
+++ 2138-Dígito-Mais-Frequente.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int most_often(int *v);
+void reset(int *v);
+
+
+void reset(int *v){
+  int i;
+  for(i=0;i<10;i++){
+    v[i] = 0;
+  }
+}
+
+
+int most_often(int *v){
+  int i, m = 0, p = 0;
+  for(i=0;i<10;i++){
+    if(v[i] >= m){
+      m = v[i];
+      p = i;
+    }
+  }
+  return p;
+}
+
+
+int main(){
+  char c;
+  int v[10], i;
+  reset(v);
+
+  while((c=getchar())!=EOF){
+    if(c>='0' && c<='9'){
+      v[((int)c)-'0']++;
+    }
+
+    else{
+      printf("%d\n", most_often(v));
+      reset(v);
+    }
+  }
+  return 0;
+}
