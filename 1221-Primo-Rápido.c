3d45d51fe1792b958a399bfa4820aeba9c55cbdc
Daniel de Souza Lima Junior <39025412+OHomemParede@users.noreply.github.com>
2021-07-31 17:39:59 -0300

    Update 1221-Primo-Rápido.c

--- 1221-Primo-Rápido.c
+++ 1221-Primo-Rápido.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 #include <math.h>
 
-int isprime(int n);
+int isprime(unsigned long long int n);
 
 
-int isprime(int n){
+int isprime(unsigned long long int n){
   unsigned int cont;
   
+  if(n<=1)
+    return 0;
+
   if(n==2) 
     return 1;
   
-  if(n%2==0) 
+  if(!(n&1)) 
     return 0;
   
   for(cont=3; cont<=pow(n,0.5); cont=cont+2){
@@ -21,15 +24,16 @@ int isprime(int n){
 }
 
 int main() {
-  int n, a, cont;
+  int a, cont;
+  unsigned long long int n;
   scanf("%d", &a); // a → quantidade de entrada
   for(cont=0;cont<a;cont++){
-    scanf("%d", &n);
+    scanf("%llu", &n);
     
     if(isprime(n))
       printf("Prime\n");
   
     else
-      printf(" Not Prime\n");
+      printf("Not Prime\n");
   }
 }
